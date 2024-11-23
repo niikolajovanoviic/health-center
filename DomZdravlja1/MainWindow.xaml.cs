bbc8c899f4d017cb98a4ea0f2ad322aaca588d71
niikolajovanoviic <nikola13521@its.edu.rs>
2024-11-23 02:24:20 +0100

    upload full project

---
+++ DomZdravlja1/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DomZdravlja1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static DomZdravljaDataContext DomZdravljaDC = new DomZdravljaDataContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+            PuniCombo();
+        }
+        private void PuniCombo()
+        {
+            var rez = from f in DomZdravljaDC.Firmes
+                      select f;
+            comboBox.ItemsSource = rez;
+        }
+        private void puniDataGrid()
+        {
+            var rez = from p in DomZdravljaDC.Pacijentis
+                      join pa in DomZdravljaDC.Paketis
+                      on p.PaketID equals pa.PaketID
+                      join f in DomZdravljaDC.Firmes
+                      on pa.FirmaID equals f.FirmaID
+                      where pa.FirmaID == ((Firme)comboBox.SelectedValue).FirmaID
+                      select new
+                      {
+                          p.PacijentID,
+                          p.Ime,
+                          p.Prezime,
+                          p.DatumIzmene,
+                          pa.Naziv,
+                          pa.Cena,
+                          p.Popust
+                      };
+            dataGrid.ItemsSource = rez;
+            textBlockUkupno.Text = rez.Count().ToString();
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            puniDataGrid();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            groupBox.IsEnabled = true;
+        }
+
+        private void btnIzmeni_Click(object sender, RoutedEventArgs e)
+        {
+            Pacijenti rez = (from p in DomZdravljaDC.Pacijentis
+                             where p.PacijentID == int.Parse(tbSifraPacijenta.Text)
+                             select p).Single();
+            rez.Popust = int.Parse(tbNoviPopust.Text);
+            rez.DatumIzmene = System.DateTime.Now.Date;
+
+            try
+            {
+                DomZdravljaDC.SubmitChanges();
+                MessageBox.Show("Uspesno ste promenili popust!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Greska: {ex.Message}");
+                throw;
+            }
+            puniDataGrid();
+            tbNoviPopust.Text = string.Empty;
+                            
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Pacijenti rez = (from p in DomZdravljaDC.Pacijentis
+                             where p.PacijentID == int.Parse(tbSifraPacijenta.Text)
+                             select p).Single();
+            DomZdravljaDC.Pacijentis.DeleteOnSubmit(rez);
+
+            try
+            {
+                DomZdravljaDC.SubmitChanges();
+                MessageBox.Show($"Uspesno ste obirsali pacijenta sa ID: {tbSifraPacijenta.Text}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Greska: {ex.Message}");
+                throw;
+            }
+            puniDataGrid();
+        }
+
+        private void radioMax_Checked(object sender, RoutedEventArgs e)
+        {
+            var max = (from p in DomZdravljaDC.Pacijentis
+                       join pak in DomZdravljaDC.Paketis
+                       on p.PaketID equals pak.PaketID
+                       where pak.FirmaID == ((Firme)comboBox.SelectedValue).FirmaID
+                       select p.Popust).Max();
+
+            var korisnici = from p in DomZdravljaDC.Pacijentis
+                      join pa in DomZdravljaDC.Paketis
+                      on p.PaketID equals pa.PaketID
+                      where pa.FirmaID == ((Firme)comboBox.SelectedItem).FirmaID && p.Popust == max
+                      select new
+                      {
+                          p.PacijentID,
+                          pa.Naziv,
+                          p.Popust
+                      };
+            listBoxPopust.ItemsSource = korisnici;
+                      
+        }
+
+        private void radioMin_Checked(object sender, RoutedEventArgs e)
+        {
+            var min = (from p in DomZdravljaDC.Pacijentis
+                       join pak in DomZdravljaDC.Paketis
+                       on p.PaketID equals pak.PaketID
+                       where pak.FirmaID == ((Firme)comboBox.SelectedValue).FirmaID
+                       select p.Popust).Min();
+
+            var korisnici = from p in DomZdravljaDC.Pacijentis
+                            join pa in DomZdravljaDC.Paketis
+                            on p.PaketID equals pa.PaketID
+                            where pa.FirmaID == ((Firme)comboBox.SelectedItem).FirmaID && p.Popust == min
+                            select new
+                            {
+                                p.PacijentID,
+                                pa.Naziv,
+                                p.Popust
+                            };
+            listBoxPopust.ItemsSource = korisnici;
+        }
+
+        private void meniUnos_Click(object sender, RoutedEventArgs e)
+        {
+            WindowUnos windowUnos = new WindowUnos();
+            windowUnos.Show();
+        }
+    }
+}
---
+++ DomZdravlja1/WindowUnos.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DomZdravlja1
+{
+    /// <summary>
+    /// Interaction logic for WindowUnos.xaml
+    /// </summary>
+    public partial class WindowUnos : Window
+    {
+        private static DomZdravljaDataContext DomZdravljaDC = new DomZdravljaDataContext();
+        public WindowUnos()
+        {
+            InitializeComponent();
+            PuniCombo();
+        }
+        private void PuniCombo()
+        {
+            var rez = (from f in DomZdravljaDC.Firmes
+                      select f).ToList();
+            cbFirma.ItemsSource = rez;
+        }
+        private void PuniPakete()
+        {
+            var rez = (from p in DomZdravljaDC.Paketis
+                      where p.FirmaID == ((Firme)cbFirma.SelectedValue).FirmaID
+                      select p).ToList();
+            cbPaket.ItemsSource = rez;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnPotvrdi_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(tbSifra.Text==string.Empty) && !(tbIme.Text == string.Empty) && !(tbPrezime.Text == string.Empty) && 
+                !(cbFirma.SelectedIndex<0) && !(cbPaket.SelectedIndex<0) && !(tbSliderValue.Text==string.Empty))
+            {
+                Pacijenti nov = new Pacijenti
+                {
+                    PacijentID = int.Parse(tbSifra.Text),
+                    Ime = tbIme.Text.ToString(),
+                    Prezime = tbPrezime.Text.ToString(),
+                    DatumIzmene = System.DateTime.Now.Date,
+                    Popust = int.Parse(tbSliderValue.Text),
+                    PaketID = ((Paketi)cbPaket.SelectedValue).PaketID
+                };
+                DomZdravljaDC.Pacijentis.InsertOnSubmit(nov);
+                try
+                {
+                    DomZdravljaDC.SubmitChanges();
+                    MessageBox.Show("Uspesno dodat u bazu!");
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Greska" + ex.Message);
+                    throw;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Popunite sva polja!");
+            }
+        }
+
+        private void cbFirma_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PuniPakete();
+        }
+
+        private void cbPaket_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+    }
+}
